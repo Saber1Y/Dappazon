926e5ca1c7891117f8f9aed39e3de1acef26ca03
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-27 14:33:34 +0100

    UPDATE: LIST UPDATED

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -1,30 +1,40 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Dappazon {  
-    string public name = "Dappazon";
-    address public Owner = msg.sender;
-}
+contract Dappazon {
+    address public owner = msg.sender;
 
+    struct Item {
+        uint256 id;
+        string name;
+        string image;
+        string catalogue;
+        uint256 cart;
+        uint256 rating;
+        uint256 stock;
+    }
 
-struct Item {
-    uint256 id;
-    string image;
-    string name;
-    string catalogue;
-    uint256 cart;
-    uint256 rating;
-    uint256 stock;
-}
+    mapping(uint256 => Item) public items;
 
-function ListProduct(
-    uint256 _id,
-    string memory _name
-    string memory _image, 
-    string memory _catalogue, 
-    uint256 _cart, 
-    uint256 _rating,
-    uint256 _stock
+    function ListProduct(
+        uint256 _id,
+        string memory _name,
+        string memory _image,
+        string memory _catalogue,
+        uint256 _cart,
+        uint256 _rating,
+        uint256 _stock
     ) public {
-        Item  memory item = Item(_id, _name, _cart, _rating, _stock, _image, _catalogue)
+        Item memory item = Item(
+            _id,
+            _name,
+            _image,
+            _catalogue,
+            _cart,
+            _rating,
+            _stock
+        );
+
+        items[_id] = item;
+    }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { describe } = require("node:test");
 
 
 
@@ -17,28 +18,34 @@ describe("Dappazon", () => {
     dappazon = await Dappazon.deploy();
 
     [deployer, buyer] = await ethers.getSigners();
-  });
 
-  // describe("Deployment", () => {
-  //   it('has a name', async () => {
-  //     const name = await dappazon.name();
-  //     expect(name).to.equal("Dappazon");
-  //   });
 
     it('Sets Owner', async () => {
       const Contract = await dappazon.Owner();
-
       expect(Contract).to.equal(deployer.address);
     });
   })
-// });
+});
+
+describe('Listing', () => {
+  beforeEach(async () => {
+    transaction = await dappazon.connect(deploy).list(
+      1,
+      "Shoes",
+      "IMAGE",
+      "Clothing",
+      5,
+      1,
+      4
+    )
+
+    await transaction.wait()
 
 
+    it('Returns List', async () => {
+      const item = await dappazon.items(1);
+      expect(item.id).to.equal(item.id);
+    });
+  })
+});
 
-// it("Dappazon", async () => {
-//   const Dappazon = await ethers.getContractFatory('Dappazon');
-//   const dappazon = await Dappazon.deploy();
-
-//   expect(dappazon.name()).to.equal("Dappazon");
-
-// })
