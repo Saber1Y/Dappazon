f52969144a5bd52ffcc19920ab07a57695150c94
ayodeji <ayodejiakintobi1@gmail.com>
2024-01-29 22:25:53 +0100

    UPDATE: IFPS IMG ADDED

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,4 +1,5 @@
 const { expect } = require("chai");
+const { describe } = require("node:test");
 // const { beforeEach } = require("node:test");
 
 const tokens = (n) => {
@@ -16,33 +17,40 @@ describe("Dappazon", () => {
     [deployer, buyer] = await ethers.getSigners();
   });
 
-  it('Sets Owner', async () => {
-    const Contract = await dappazon.owner();
-    expect(Contract).to.equal(deployer.address);
-  });
+  // it('Sets Owner', async () => {
+  //   const Contract = await dappazon.owner();
+  //   expect(Contract).to.equal(deployer.address);
+  // });
+  describe("Deployment", () => {
+    it('Sets Deployment', async () => {
+      expect(await dappazon.owner()).to.equal(deployer.address);
+    })
+  })
 
-  describe('Listing', () => {
-    let transaction;
+  describe("Listing", () => {
+    let transaction
+
+    const ID = 1
+    const NAME = "Shoes"
+    const CATEGORY = "Clothing"
+    const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+    const COST = tokens(1)
+    const RATING = 4
+    const STOCK = 5
+
 
     beforeEach(async () => {
-
-      transaction = await dappazon.connect(deployer).list(
-        1,
-        "Shoes",
-        "IMAGE",
-        "Clothing",
-        5,
-        1,
-        4
-      );
+      transaction = await dappazon.connect(deployer).list(1, 'Shoes', "IMAGE", 1, 4, 5)
 
       await transaction.wait();
     });
 
-    it('Returns Items attributes', async () => {
+    it('Returns Item Attribute', async () => {
       const item = await dappazon.items(1);
       expect(item.id).to.equal(1);
-    });
+    })
 
-  });
+  })
+
+
 });
