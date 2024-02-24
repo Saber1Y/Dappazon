4cbc757105d25c510900e1c18e3d214a703ddc8e
ayodeji <ayodejiakintobi1@gmail.com>
2024-02-24 10:49:04 +0100

    UPDATE: LOGGED ITEMS

--- src/App.js
+++ src/App.js
@@ -12,50 +12,52 @@ import config from './config.json'
 function App() {
 
   const [account, setAccount] = useState(null);
+
   const [provider, setProvider] = useState(null);
+
   const [dappazon, setDappazon] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+
 
 
   const loadBlockchainData = async () => {
-    //creats connection to the chain
+
     const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider);
+    setProvider(provider)
+    const network = await provider.getNetwork()
 
-    //Network on chain
-
-    const network = await provider.getNetwork();
-    console.log(network);
-
-    const dappazon = new ethers.Contract(
-      config[network.chainId].dappazon.address,
-      provider,
-      Dappazon
-    )
+    const dappazon = new ethers.Contract(config[network.chainId].dappazon.address, Dappazon, provider)
     setDappazon(dappazon)
 
     const items = []
 
     for (let i = 0; i < 9; i++) {
       const item = await dappazon.items(i + 1)
-      items.psuh(item)
+      console.log(item);
+      items.push(item)
     }
 
     console.log(items);
 
     const electronics = items.filter((item) => item.category === 'electronics')
-    console.log(electronics)
-
+    console.log(electronics);
+    setElectronics(electronics);
   }
 
-
-  // useEffect(() => {
-  //   loadBlockchainData();
-  // }, []);
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
 
   return (
     <div className=''>
       <Navigation account={account} setAccount={account} />
       <h2 className=''>Dappazon's Best Picks</h2>
+
+      {electronics && (
+        <p>Products</p>
+      )}
+
     </div>
   );
 }
